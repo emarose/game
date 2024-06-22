43fc86671f484ef18c545d311fa0fb8fd86f7432
Emanuel Amerio <83380305+emarose@users.noreply.github.com>
2024-06-22 11:53:35 -0300

    rebuild

--- src/utils/generateChatCompletion.jsx
+++ src/utils/generateChatCompletion.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 
 export const generateChatCompletion = async (content) => {
   const baseUrl = "https://api.groq.com/openai/v1";
+  const apiKey = "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";
+
   try {
     // Define the message to send
     const message = {
