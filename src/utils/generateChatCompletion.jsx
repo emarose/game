062243410dd53cc33ff959ab50711d6f2cc12d83
Emanuel Amerio <83380305+emarose@users.noreply.github.com>
2024-06-22 11:50:47 -0300

    cehck}

--- src/utils/generateChatCompletion.jsx
+++ src/utils/generateChatCompletion.jsx
@@ -5,7 +5,6 @@ import { fetchGroqData } from "./groqClient";
 export const generateChatCompletion = async (content) => {
   const baseUrl = "https://api.groq.com/openai/v1";
   const apiKey = "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";
-  console.log(content);
   try {
     // Define the message to send
     const message = {
